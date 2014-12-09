2a568b674481e29e95bd3ac289eb06d1c4ecc5a9
wjpak_mcp <jadenpak897@icloud.com>
2014-12-09 19:23:20 +0900

    Initial Commit

---
+++ T01_SimpleTouchTests/T01_SimpleTouchTests.m
@@ -0,0 +1,40 @@
+//
+//  T01_SimpleTouchTests.m
+//  T01_SimpleTouchTests
+//
+//  Created by wjpak on 12/9/14.
+//  Copyright (c) 2014 jdp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface T01_SimpleTouchTests : XCTestCase
+
+@end
+
+@implementation T01_SimpleTouchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
